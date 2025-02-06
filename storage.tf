c43a1a5abf6f08220577c781dc66902f9bdb71bd
ajirijames <ajirijames1@gmail.com>
2025-02-05 20:57:30 -0500

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_account" "example" {
-  name                     = "ajirijamesstorage"   # Storage account name must be globally unique
-  resource_group_name      = azurerm_resource_group.example.name
+  name                     = "ajirijamesstorage"   
+  resource_group_name      = azurerm_resource_group.ajiri.name
   location                = azurerm_resource_group.canadacentral.location
   account_tier             = "Standard"
   account_replication_type = "LRS"   # Locally redundant storage
