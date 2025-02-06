0e484297ca1c0ca2518774a49eb14970868ff2bd
ajirijames <ajirijames1@gmail.com>
2025-02-05 21:18:27 -0500

    Update appserviceplan.tf

--- appserviceplan.tf
+++ appserviceplan.tf
@@ -10,10 +10,10 @@ resource "azurerm_app_service_plan" "app2025" {
 }
 
 resource "azurerm_function_app" "functionapp2025" {
-  name                       = "ajirifunctionapp"  # Function App name must be globally unique
+  name                       = "ajirifunctionapp"
   location                   = azurerm_resource_group.canadacentral.location
   resource_group_name        = azurerm_resource_group.mcit_resource_group_ajiri.name
   app_service_plan_id        = azurerm_app_service_plan.appsp2025.id
-  storage_account_name      = azurerm_storage_account.example.name
+  storage_account_name      = azurerm_storage_account.ajirijamesstorage.name
   storage_account_access_key = azurerm_storage_account.example.primary_access_key
-  os_type                    = "Linux"  
+  os_type                    = "Windows"  
